50df0da0a33ff20e0a27bade0e277520acbcd1a1
Oleksandr Baior <bayor.olexandr@gmail.com>
2025-04-05 21:25:39 +0200

    Add test EA-1

--- cypress/pages/sign_up.page.ts
+++ cypress/pages/sign_up.page.ts
@@ -1,14 +1,13 @@
 import { first } from "cypress/types/lodash";
 
 class Sign_up {
-    form = {
-        sign_up_for_an_account_btn: () => cy.get('.fi-simple-header-subheading > .fi-link > .font-semibold'),
-        first_name_inp: () => cy.get('input#data\\.name'),
-        last_name_inp: () => cy.get('input#data\\.lastname'),
-        sign_up_btn: () => cy.get('.fi-btn')
-       
-   
-    };
+  form = {
+    sign_up_for_an_account_btn: () =>
+      cy.get(".fi-simple-header-subheading > .fi-link > .font-semibold"),
+    first_name_inp: () => cy.get("input#data\\.name"),
+    last_name_inp: () => cy.get("input#data\\.lastname"),
+    sign_up_btn: () => cy.get(".fi-btn"),
+  };
 }
 
 export default new Sign_up();
