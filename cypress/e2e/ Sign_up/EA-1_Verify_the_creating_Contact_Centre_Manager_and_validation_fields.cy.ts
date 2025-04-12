d25503cf6fee1c6b1a840c8d7050839706bd6c34
Oleksandr Baior <bayor.olexandr@gmail.com>
2025-04-12 21:44:29 +0200

    fix EA-1

--- cypress/e2e/ Sign_up/EA-1_Verify_the_creating_Contact_Centre_Manager_and_validation_fields.cy.ts
+++ cypress/e2e/ Sign_up/EA-1_Verify_the_creating_Contact_Centre_Manager_and_validation_fields.cy.ts
@@ -2,7 +2,7 @@ import { roles } from '../../support/roles';
 import sign_up from '../../pages/sign_up.page';
 import { randomValue } from '../../support/utils';
 import constants from '../../resourcers/constants.json';
-import enpoits from '../../resourcers/endpoints.json';
+import endpoits from '../../resourcers/endpoints.json';
 
 describe('EA-1', () => {
   it('Verify the creating Contact Centre Manager and validation fields', () => {
@@ -12,90 +12,64 @@ describe('EA-1', () => {
     const name = randomValue('name') as string;
     const phone = `201555${randomValue('phone')}` as string;
 
-    const CCM = roles.CCM.url;
-    cy.visit(CCM);
+    cy.visit(roles.CCM.url);
 
     // Verify Sign up and Sign in buttons
     sign_up.form.sign_up_for_an_account_btn().click();
-    cy.url().should('include', enpoits.register);
-
+    cy.url().should('include', endpoits.register);
+    sign_up.verifyUrl('register');
     sign_up.form.sign_up_to_an_account_btn().click();
-    cy.url().should('include', enpoits.login);
-
+    sign_up.verifyUrl('login');
     sign_up.form.sign_up_for_an_account_btn().click();
 
     // First name
-    sign_up.form.sign_up_btn().click();
-    sign_up.form.first_name_inp().should('have.css', 'border-color', constants.styleElement.errorBorder);
-
+    sign_up.verifyNextEmptyField(() => sign_up.form.first_name_inp_border());
     sign_up.form.first_name_inp().type(firstName);
-    sign_up.form.first_name_inp().should('have.value', firstName);
-    sign_up.form.first_name_inp().should('have.css', 'border-color', constants.styleElement.activeBorder);
+    sign_up.verifyEnteredValue(sign_up.form.first_name_inp(), firstName);
 
     // Last name
-    sign_up.form.sign_up_btn().click();
-    sign_up.form.last_name_inp().should('have.css', 'border-color', constants.styleElement.errorBorder);
-
+    sign_up.verifyNextEmptyField(() => sign_up.form.last_name_inp_border());
     sign_up.form.last_name_inp().type(lastName);
-    sign_up.form.last_name_inp().should('have.value', lastName);
-    sign_up.form.last_name_inp().should('have.css', 'border-color', constants.styleElement.activeBorder);
+    sign_up.verifyEnteredValue(sign_up.form.last_name_inp(), lastName);
 
     // Email
-    sign_up.form.sign_up_btn().click();
-    sign_up.form.email_inp().should('have.css', 'border-color', constants.styleElement.errorBorder);
-
-    sign_up.verifyInvalidEmails(constants.invalidEmails);
-
+    sign_up.verifyNextEmptyField(() => sign_up.form.email_inp_border());
+    sign_up.verifyInvalidEmails(sign_up.form.email_inp, sign_up.form.email_inp_border, constants.invalidEmails);
     sign_up.form.email_inp().type(email);
-    sign_up.form.email_inp().should('have.value', email);
-    sign_up.form.email_inp().should('have.css', 'border-color', constants.styleElement.activeBorder);
+    sign_up.verifyEnteredValue(sign_up.form.email_inp(), email);
 
     // Phone
-    sign_up.form.sign_up_btn().click();
-    sign_up.form.phone_inp().should('have.css', 'border-color', constants.styleElement.errorBorder);
-
+    sign_up.verifyNextEmptyField(() => sign_up.form.phone_inp_border());
     sign_up.form.country_select().click();
     sign_up.form.search_input().type(constants.country[0]);
-
     sign_up.form.first_contry_code().click();
     sign_up.form.phone_inp().type(phone);
     sign_up.verfiyEnteredPhoneNumber(phone);
-
     sign_up.form.phone_inp().should('have.css', 'border-color', constants.styleElement.activeBorder);
 
     // Contact center name
-    sign_up.form.sign_up_btn().click();
-    sign_up.form.contact_center_name_inp().should('have.css', 'border-color', constants.styleElement.errorBorder);
-
+    sign_up.verifyNextEmptyField(() => sign_up.form.contact_center_name_inp_border());
     sign_up.form.contact_center_name_inp().type(name);
-    sign_up.form.contact_center_name_inp().should('have.value', name);
-    sign_up.form.contact_center_name_inp().should('have.css', 'border-color', constants.styleElement.activeBorder);
+    sign_up.verifyEnteredValue(sign_up.form.contact_center_name_inp(), name);
 
     // Password
-    sign_up.form.sign_up_btn().click();
-    sign_up.form.password_inp().should('have.css', 'border-color', constants.styleElement.errorBorder);
-
-    sign_up.verifyInvalidPasswords(constants.invalidPasswords);
-
-    sign_up.form.password_inp().type(constants.validPassword[0]);
-    sign_up.form.password_inp().should('have.value', constants.validPassword[0]);
-
-    sign_up.form.password_confirm_inp().type(constants.validPassword[1]);
-    sign_up.form.password_confirm_inp().should('have.value', constants.validPassword[1]);
-
+    sign_up.verifyNextEmptyField(() => sign_up.form.password_inp_border());
+    sign_up.verifyInvalidPasswords(sign_up.form.password_inp, sign_up.form.password_inp_border, constants.invalidPasswords);
+    sign_up.inputPassword(sign_up.form.password_inp(), constants.validPassword[0]);
+    // Confrm password
+    sign_up.verifyNextEmptyField(() => sign_up.form.password_confirm_inp_border());
+    sign_up.inputPassword(sign_up.form.password_confirm_inp(), constants.validPassword[1]);
     sign_up.form.sign_up_btn().click();
     sign_up.form.password_error().should('contain', constants.errorMessages.passwordError);
     sign_up.form.password_inp().should('have.css', 'border-color', constants.styleElement.errorRedBorder);
-
     sign_up.form.password_confirm_inp().clear();
-    sign_up.form.password_confirm_inp().type(constants.validPassword[0]);
-    sign_up.form.password_confirm_inp().should('have.value', constants.validPassword[0]);
+    sign_up.inputPassword(sign_up.form.password_confirm_inp(), constants.validPassword[0]);
 
     // Verify password visibility eyes
     sign_up.verifyHidderVisiblePassword();
 
     // Sign up
     sign_up.form.sign_up_btn().click();
-    cy.url().should('include', enpoits.login);
+    sign_up.verifyUrl('login');
   });
 });
--- cypress/pages/sign_up.page.ts
+++ cypress/pages/sign_up.page.ts
@@ -1,39 +1,66 @@
 import constants from '../resourcers/constants.json';
+import endpoits from '../resourcers/endpoints.json';
 
 class Sign_up {
   form = {
     sign_up_to_an_account_btn: () => cy.get('.fi-link > .font-semibold'),
     sign_up_for_an_account_btn: () => cy.get('.fi-simple-header-subheading > .fi-link > .font-semibold'),
-    first_name_inp: () => cy.get('input#data\\.name'),
-    last_name_inp: () => cy.get('input#data\\.lastname'),
     sign_up_btn: () => cy.get('.fi-btn'),
+    // first name
+    first_name_inp: () => cy.get('input#data\\.name'),
+    first_name_inp_border: () => cy.get('#form > div:nth-child(1) > div:nth-child(1) > div > div > div:nth-child(2) > div'),
+    // last name
+    last_name_inp: () => cy.get('input#data\\.lastname'),
+    last_name_inp_border: () => cy.get('#form > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(2) > div'),
+    // email
     email_inp: () => cy.get('input#data\\.email'),
+    email_inp_border: () => cy.get('#form > div:nth-child(1) > div:nth-child(3) > div > div > div:nth-child(2) > div'),
+    // phone
     phone_inp: () => cy.get('input#data\\.phone'),
+    phone_inp_border: () => cy.get('#form > div:nth-child(1) > div:nth-child(4) > div > div > div:nth-child(2) > div'),
     country_select: () => cy.get('.iti__selected-country-primary'),
     search_input: () => cy.get('.iti__search-input'),
     first_contry_code: () => cy.get('#iti-0__item-gb'),
+    // contact center name
     contact_center_name_inp: () => cy.get('input#data\\.contact_centre_name'),
+    contact_center_name_inp_border: () => cy.get('#form > div:nth-child(1) > div:nth-child(5) > div > div > div:nth-child(2) > div'),
+    // password
     password_inp: () => cy.get('input#data\\.password'),
+    password_inp_border: () => cy.get('#form > div:nth-child(1) > div:nth-child(6) > div > div > div:nth-child(2) > div'),
     password_confirm_inp: () => cy.get('input#data\\.passwordConfirmation'),
+    password_confirm_inp_border: () => cy.get('#form > div:nth-child(1) > div:nth-child(7) > div > div > div:nth-child(2) > div'),
     password_error: () => cy.get('.fi-fo-field-wrp-error-message'),
     password_eye: () => cy.get('div.fi-input-wrp-suffix button.fi-icon-btn.relative.flex.items-center').eq(0),
     password_confirm_eye: () => cy.get('div.fi-input-wrp-suffix button.fi-icon-btn.relative.flex.items-center').eq(2),
   };
-  verifyInvalidValues(list: Array<string>, field: Cypress.Chainable<JQuery<HTMLElement>>): void {
-    list.forEach((element) => {
-      field.type(element);
-      field.should('have.css', 'border-color', constants.styleElement.errorBorder);
+
+  verifyUrl(value: 'login' | 'register'): void {
+    cy.url().should('include', value);
+  }
+
+  verifyInvalidValues(element: () => Cypress.Chainable<JQuery<HTMLElement>>, border: () => Cypress.Chainable<JQuery<HTMLElement>>, lists: Array<string>): void {
+    lists.forEach((el) => {
+      element().type(el);
+      this.verifyErrorBorderColor(border);
       this.form.sign_up_btn().click();
-      field.clear();
+      element().clear();
     });
   }
 
-  verifyInvalidEmails(emails: Array<string>): void {
-    this.verifyInvalidValues(emails, this.form.email_inp());
+  verifyInvalidEmails(
+    element: () => Cypress.Chainable<JQuery<HTMLElement>>,
+    border: () => Cypress.Chainable<JQuery<HTMLElement>>,
+    emails: Array<string>,
+  ): void {
+    this.verifyInvalidValues(element, border, emails);
   }
 
-  verifyInvalidPasswords(passwords: Array<string>): void {
-    this.verifyInvalidValues(passwords, this.form.password_inp());
+  verifyInvalidPasswords(
+    element: () => Cypress.Chainable<JQuery<HTMLElement>>,
+    border: () => Cypress.Chainable<JQuery<HTMLElement>>,
+    passwords: Array<string>,
+  ): void {
+    this.verifyInvalidValues(element, border, passwords);
   }
 
   verifyPasswordEyeVisibility(visible: boolean): void {
@@ -57,6 +84,29 @@ class Sign_up {
       .then((value: string) => {
         expect(value.replace(/\s/g, '')).to.equal(`0${phone}`);
       });
+  }
+
+  verifyNextEmptyField(element: () => Cypress.Chainable<JQuery<HTMLElement>>): void {
+    this.form.sign_up_btn().click();
+    this.verifyBorderColor(element);
+  }
+
+  verifyEnteredValue(element: Cypress.Chainable<JQuery<HTMLElement>>, value: string): void {
+    element.should('have.value', value);
+    element.should('have.css', 'border-color', constants.styleElement.activeBorder);
+  }
+
+  inputPassword(element: Cypress.Chainable<JQuery<HTMLElement>>, value: string): void {
+    element.type(value);
+    element.should('have.value', value);
+  }
+
+  verifyBorderColor(element: () => Cypress.Chainable<JQuery<HTMLElement>>): void {
+    this.verifyErrorBorderColor(element);
+  }
+
+  verifyErrorBorderColor(element: () => Cypress.Chainable<JQuery<HTMLElement>>): void {
+    element().should('have.css', 'box-shadow').and('include', 'rgb(217, 119, 6)');
   }
 }
 export default new Sign_up();
